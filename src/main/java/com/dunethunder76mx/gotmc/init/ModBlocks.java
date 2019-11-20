321883d69d8154f25563a5b55e0e898bf2db5ace
kyjor <kyjor@DESKTOP-UIL8LK4.home.jo.>
2019-11-20 15:56:34 -0600

    Tests

--- src/main/java/com/dunethunder76mx/gotmc/init/ModBlocks.java
+++ src/main/java/com/dunethunder76mx/gotmc/init/ModBlocks.java
@@ -19,10 +19,13 @@ public class ModBlocks
 {
 	public static final List<Block> BLOCKS = new ArrayList<Block>();
 	
+	//Blocks
 	public static final Block TARGARYAN_BRICK_BLOCK = new TargaryanBrick("targaryan_brick_block", Material.ROCK);
 	public static final Block DRAGONSTONE_ROCK_BLOCK = new DragonstoneRock("dragonstone_rock_block", Material.ROCK);
 	public static final Block DRAGONSTONE_PILLAR_BLOCK = new DragonstonePillar("dragonstone_pillar_block", Material.ROCK);
 	public static final Block TARGARYAN_CARVED_BRICK = new TargaryanCarvedBrick("targaryan_carved_brick", Material.ROCK);
 	public static final Block DRAGONSTONE_CARVED_BRICK = new DragonstoneCarvedBrick("dragonstone_carved_brick", Material.ROCK);
 	public static final Block RED_KEEP_BRICK = new RedKeepBrick("red_keep_brick", Material.ROCK);
-}
+	
+	
+	}
