c97e777c932aef6c5b758e5545700c4018e8bc49
kyjor <kyjor@DESKTOP-UIL8LK4.home.jo.>
2019-12-16 22:47:31 -0600

    Needle Changes

--- src/main/java/com/dunethunder76mx/gotmc/init/ModItems.java
+++ src/main/java/com/dunethunder76mx/gotmc/init/ModItems.java
@@ -51,7 +51,7 @@ public class ModItems
 	
 	//Materials
 	public static final ToolMaterial MATERIAL_STEEL_INGOT = EnumHelper.addToolMaterial("material_steel_ingot", 2, 550, 6.5F, 3.0F, 14);
-	public static final ToolMaterial MATERIAL_NEEDLE = EnumHelper.addToolMaterial("material_needle", 2, 1200, 3.0F, 5.0F, 15);
+	public static final ToolMaterial MATERIAL_NEEDLE = EnumHelper.addToolMaterial("material_needle", 0, 7500, 0.5F, 5.0F, 22);
 	public static final ToolMaterial MATERIAL_DRAGONGLASS = EnumHelper.addToolMaterial("material_dragonglass", 1, 25, 1.0F, 1.0F, 20);
 	public static final ToolMaterial MATERIAL_TRAINING = EnumHelper.addToolMaterial("material_training", 1, 450, 0.5F, -1.5F, 5);
 	public static final ToolMaterial MATERIAL_DRAGONSTONE_BATTLEAXE = EnumHelper.addToolMaterial("material_dragonstone_battleaxe", 0, 550, 1.5F, 2.0F, 14);
